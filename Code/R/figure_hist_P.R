4c69ec8350dbecec83d8ca4dccd6265daffa9a1d
Runze Tang <tangrunze@gmail.com>
2017-04-04 21:40:10 -0400

    Histogram of P added.

---
+++ Code/R/figure_hist_P.R
@@ -0,0 +1,36 @@
+rm(list = ls())
+setwd("/Users/Runze/Documents/GitHub/LLG/Code/R")
+
+dataName = "desikan"
+
+source("function_collection.R")
+require(ggplot2)
+
+tmpList = read_data(dataName, DA=F, newGraph=F)
+A_all = tmpList[[1]]
+n = tmpList[[2]]
+M = tmpList[[3]]
+rm(tmpList)
+P = add(A_all)/M
+diag(P) = NA
+pVec = c(P)
+pVec = pVec[!is.na(pVec)]
+pVec
+# hist(pVec)
+
+(sum(pVec == 0))/n/(n-1)
+(sum(pVec == 1))/n/(n-1)
+
+df <- data.frame(p=pVec)
+# label_y <- with(df, .75*yMax+.25*yMin)
+
+pp <- ggplot(df,aes(x=p))+
+  geom_histogram()+
+  xlab("P") + ylab("count")+
+  theme(panel.grid.major = element_line(colour="grey95"),
+        panel.grid.minor = element_blank())+
+  theme(panel.background = element_rect(fill = 'white', colour = 'grey70'))+
+  theme(legend.position="none")+
+  ggtitle("Histogram of P for Desikan")
+
+pp
